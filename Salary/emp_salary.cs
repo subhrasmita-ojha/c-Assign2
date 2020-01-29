98641a75574d908a1adc5450265e9be0aa0504a4
subhrasmita-ojha <60036690+subhrasmita-ojha@users.noreply.github.com>
2020-01-29 12:55:08 +0530

    Add files via upload

---
+++ Salary/emp_salary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class emp_salary
+    {
+        static void Main(string[] args)
+        {
+            Employee E1 = new Hr();
+            Employee E2 = new Developer();
+            Console.WriteLine("The Salary Of HR :" + E1.CalSalary());
+            Console.WriteLine("The Slary Of Developer :" + E2.CalSalary());
+            Console.ReadKey();
+        }
+    }
+}
